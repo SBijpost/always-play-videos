0472c33e47037642e7ad791bd70b1d9eedb63750
unknown <sembijpost@gmail.com>
2024-09-06 21:12:45 +0200

    1.0

---
+++ src/app.tsx
@@ -0,0 +1,47 @@
+async function main() {
+    if (
+        !(
+            Spicetify.React &&
+            Spicetify.ReactDOM &&
+            Spicetify.SVGIcons &&
+            Spicetify.Platform
+        )
+    ) {
+        setTimeout(main, 10);
+        return;
+    }
+
+    let alwaysShowVideo = false;
+
+    const button = new Spicetify.Playbar.Button(
+        "Always play video",
+        "projector",
+        () => {
+            toggleAlwaysShowVideo();
+            button.element.style.color = alwaysShowVideo ? "red" : "rgba(255,255,255,.7)";
+        },
+        false,
+        false,
+    );
+
+    async function switchToVideoIfExists() {
+        const itemHasAssociatedVideo = Spicetify.Player.data.item?.hasAssociatedVideo!;
+        if (itemHasAssociatedVideo && Spicetify.Player.data.item.mediaType !== "video") {
+            await Spicetify.Platform.PlayerAPI.sendSignal("switch-to-video", 1);
+        }
+    }
+
+    function toggleAlwaysShowVideo() {
+        alwaysShowVideo = !alwaysShowVideo;
+        if (alwaysShowVideo) {
+            switchToVideoIfExists();
+            Spicetify.Player.addEventListener("songchange", switchToVideoIfExists)
+        } else {
+            Spicetify.Player.removeEventListener("songchange", switchToVideoIfExists);
+        }
+    }
+
+
+}
+
+export default main;
